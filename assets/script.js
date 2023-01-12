ce7f02343d09792ceb17f7f336efb09c7b9991fb
Cole Hartkopp <chsoccer4@outlook.com>
2023-01-11 18:38:33 -0600

    added current date to js

--- assets/script.js
+++ assets/script.js
@@ -20,4 +20,6 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  var todayDate = dayjs().format('dddd, MMMM, DD');
+  $("#currentDay").html(todayDate);
 });
